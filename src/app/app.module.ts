28cd865ba10a2fff3ed1346698ce3622c177ffd6
Juan Diego Gómez <77864382+Juandieruiz@users.noreply.github.com>
2022-03-01 20:18:36 +0100

    init Router Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { BlogComponent } from './Pages/blog/blog.component';
+import { FormularioComponent } from './Pages/formulario/formulario.component';
+import { PruebasComponent } from './Pages/pruebas/pruebas.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     SliderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormularioComponent,
+    PruebasComponent
   ],
   imports: [
     BrowserModule,
