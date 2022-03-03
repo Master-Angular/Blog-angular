bb3283a980a695a625f9e3a9a62393c2b00764a5
Juan Diego Gómez <77864382+Juandieruiz@users.noreply.github.com>
2022-03-03 21:32:10 +0100

    Párametros por URL

--- src/app/Pages/pruebas/pruebas.component.ts
+++ src/app/Pages/pruebas/pruebas.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+// Imports para el manejo de rutas
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-pruebas',
   templateUrl: './pruebas.component.html',
   styleUrls: ['./pruebas.component.css']
 })
 export class PruebasComponent implements OnInit {
+    
+  public param1!: string; // Variable para guardar el párametro 1
+  public param2!: string; // Variable para guardar el párametro 2
 
-  constructor() { }
+
+  constructor(
+    private _route: ActivatedRoute, // Para recibir parametros de la url
+    private _router: Router // Para navegar a otras url
+  ) { }
 
   ngOnInit(): void {
+
+    this._route.params.subscribe((params: Params) => { // Recibir parametros de la url
+      this.param1 = params['param1'] // Para ver los parametros en la consola
+      this.param2 = params['param2'] 
+    });
+
   }
 
 }
